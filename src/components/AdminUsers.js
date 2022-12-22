80910faa1836bcd61d256a919b278d1cc7680cce
Eliana Ayarachi <elianaayarachi@gmail.com>
2022-12-21 21:06:04 -0400

    filtros

--- src/components/AdminUsers.js
+++ src/components/AdminUsers.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
 
 const AdminUsers = () => {
   const [users, setUsers] = useState([]);
--- src/components/filters.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const filters = () => {
-  return (
-    <div>filters</div>
-  )
-}
-
-export default filters
